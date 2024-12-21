747ee57d2f37a181a4dff7b364c71577a72d4931
magenka <magen8911@gmail.com>
2024-12-21 10:11:11 +0200

    ThankYou final

--- src/pages/ThankYou.jsx
+++ src/pages/ThankYou.jsx
@@ -1,15 +1,21 @@
+import React from "react";
 import { Link } from "react-router-dom";
+import "../css/ThankYou.css"; // CSS for the Thank You page
+import logo from "../assets/logo.png"; // Import the BarkBuddy logo
 
 const ThankYou = () => {
   return (
-    <div style={{ textAlign: "center", marginTop: "20px" }}>
-      <h1>Thank you for registering with BarkBuddy!</h1>
-      <p>Your account has been created successfully.</p>
-      <Link to="/login">
-        <button style={{ marginTop: "10px", padding: "10px 20px", fontSize: "16px" }}>
-          Click here to Login
-        </button>
-      </Link>
+    <div className="thankyou-container">
+      <div className="thankyou-card">
+        <img src={logo} alt="BarkBuddy Logo" className="thankyou-logo" />
+        <h1 className="thankyou-header">Thank You for Registering!</h1>
+        <p className="thankyou-message">
+          Your account has been created successfully. We are excited to have you as part of the BarkBuddy community!
+        </p>
+        <Link to="/login">
+          <button className="thankyou-button">Click Here to Login</button>
+        </Link>
+      </div>
     </div>
   );
 };
